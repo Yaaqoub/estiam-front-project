18ac8bd278b23fbeddba678d0fe0d76fc416ca70
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2025-01-22 09:21:14 +0100

    update

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,11 +9,42 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
+interface Values {
+  email: string;
+  password: string;
+  submit: null;
+}
 export function LoginForm({
                             className,
                             ...props
                           }: React.ComponentPropsWithoutRef<"div">) {
+
+  const initialValues: Values = {
+    email: "",
+    password: "",
+    submit: null
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email().required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values, helpers) => {
+      try {
+        console.log(values);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -31,7 +62,6 @@ export function LoginForm({
                 <Input
                   id="email"
                   type="email"
-                  placeholder="m@example.com"
                   required
                 />
               </div>
